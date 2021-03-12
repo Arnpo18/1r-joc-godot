0152aea11dbf3bd8b5eb9c15df504afe417d8746
Arnpo18 <79510111+Arnpo18@users.noreply.github.com>
2021-03-12 19:09:09 +0100

    12-3-2021 1r joc completat
    soc sexy

--- joc/KinematicBody2D.gd
+++ joc/KinematicBody2D.gd
@@ -1,28 +1,29 @@
 extends KinematicBody2D
-var velocitat := Vector2(0,0)
-const g = 50
-const moviment = 400
-func _ready():
-	pass 
-func _process(delta):
-	velocitat=Vector2.ZERO
+var moviment = Vector2()
+const gravetat = 50
+const velocitat = 400
+var esquerra = load('imatges/a/player_14.png')
+var dreta = load('imatges/a/player_11.png')
+func _physics_process(_delta):
+	
+	moviment.y += gravetat
+	
 	if Input.is_action_pressed("ui_right"):
-		velocitat += Vector2.RIGHT
-	if Input.is_action_pressed("ui_left"):
-		velocitat += Vector2.LEFT
-	if Input.is_action_pressed("ui_up"):
-		position.y += 100* delta
-	velocitat = velocitat.normalized() * moviment
-	move_and_slide(velocitat, Vector2.UP)
+		moviment.x = velocitat
+		$Sprite.texture=dreta
+	elif Input.is_action_pressed("ui_left"):
+		moviment.x = -velocitat
+		$Sprite.texture=esquerra
+	else:
+		moviment.x=0
 	if is_on_floor():
-		print('sóc a terra')
-	if is_on_wall():
-		print('sóc a una paret')
-	if is_on_ceiling():
-		print('sóc al sostre')
+		if Input.is_action_just_pressed("ui_up"):
+			moviment.y = -1000
+			
+	moviment=move_and_slide(moviment, Vector2.UP)
 
-var v = Vector2(1,1)
-func _physics_process(delta):
-	v.y += delta * g
-	var mov = v * delta
-	move_and_collide(mov)
+		
+
+
+	
+
