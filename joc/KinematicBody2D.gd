23c9292c0b209bd6b67af28fb63fa36f1cc8990d
Arnpo18 <79510111+Arnpo18@users.noreply.github.com>
2021-03-12 15:32:44 +0100

    12/3/2021

--- joc/KinematicBody2D.gd
+++ joc/KinematicBody2D.gd
@@ -1,26 +1,28 @@
 extends KinematicBody2D
 var velocitat := Vector2(0,0)
-export var velocitat_max = 750
-
+const g = 50
+const moviment = 400
 func _ready():
-	
-	position = Vector2(200,200)  
-	#flip_v=true
-
+	pass 
 func _process(delta):
 	velocitat=Vector2.ZERO
-	 #delta temps entre successives
 	if Input.is_action_pressed("ui_right"):
 		velocitat += Vector2.RIGHT
 	if Input.is_action_pressed("ui_left"):
 		velocitat += Vector2.LEFT
 	if Input.is_action_pressed("ui_up"):
-		velocitat += Vector2.UP
-	if Input.is_action_pressed("ui_down"):
-		velocitat += Vector2.DOWN
-	if Input.is_action_pressed("gira"):
-		rotation_degrees+=15
-	if Input.is_action_pressed("gira2"):
-		rotation_degrees-=15
-	velocitat = velocitat.normalized() * velocitat_max
-	position += velocitat * delta
+		position.y += 100* delta
+	velocitat = velocitat.normalized() * moviment
+	move_and_slide(velocitat, Vector2.UP)
+	if is_on_floor():
+		print('sóc a terra')
+	if is_on_wall():
+		print('sóc a una paret')
+	if is_on_ceiling():
+		print('sóc al sostre')
+
+var v = Vector2(1,1)
+func _physics_process(delta):
+	v.y += delta * g
+	var mov = v * delta
+	move_and_collide(mov)
--- joc/sprite1_code.gd
+++ joc/sprite1_code.gd
@@ -1,13 +1,3 @@
 extends Sprite
-var x=0
-var mama = load("res://mamado.jpg")
-var kirby = load("res://kirby.png")
-func _process(delta):
-	if Input.is_action_just_pressed("mama"):
-		x += 1
-		if x % 2 !=0:
-			texture=mama
-			
-		else:
-			texture=kirby
+
 			
