f217b4c7a6444ca8277d2ddfa0046b6d9d6470aa
Arnpo18 <79510111+Arnpo18@users.noreply.github.com>
2021-03-13 12:19:08 +0100

    13-3-2021 1r joc
    XD

--- joc/KinematicBody2D.gd
+++ joc/KinematicBody2D.gd
@@ -1,21 +1,41 @@
 extends KinematicBody2D
 var moviment = Vector2()
 const gravetat = 50
-const velocitat = 400
-var esquerra = load('imatges/a/player_14.png')
+const velocitat = 425
 var dreta = load('imatges/a/player_11.png')
+var d = load('imatges/a/player_23.png')
+var esquerra = load('imatges/a/player_14.png')
+
 func _physics_process(_delta):
 	
 	moviment.y += gravetat
 	
 	if Input.is_action_pressed("ui_right"):
 		moviment.x = velocitat
-		$Sprite.texture=dreta
+		if moviment.x > 0:
+			get_node("Sprite").hide()
+			get_node("AnimatedSprite").show()
+			$AnimatedSprite.play('dreta')
+		else:
+			get_node("Sprite").show()
+			get_node("AnimatedSprite").hide()
+			$Sprite.texture=load(dreta)
+			
 	elif Input.is_action_pressed("ui_left"):
 		moviment.x = -velocitat
-		$Sprite.texture=esquerra
+		if moviment.x<0:
+			get_node("Sprite").hide()
+			get_node("AnimatedSprite").show()
+			$AnimatedSprite.play('esquerra')
+		else:
+			get_node("Sprite").show()
+			get_node("AnimatedSprite").hide()
+			$Sprite.texture=load(esquerra)	
 	else:
 		moviment.x=0
+		get_node("Sprite").show()
+		get_node("AnimatedSprite").hide()
+		$Sprite.texture=d
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			moviment.y = -1000
