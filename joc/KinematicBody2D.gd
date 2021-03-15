ca92509fe67f84a1eab2bc3013a20c54f6f1b6f4
Arnpo <arnau18pons@gmail.com>
2021-03-15 14:31:21 +0100

    <a

--- joc/KinematicBody2D.gd
+++ joc/KinematicBody2D.gd
@@ -2,40 +2,22 @@ extends KinematicBody2D
 var moviment = Vector2()
 const gravetat = 50
 const velocitat = 425
-var dreta = load('imatges/a/player_11.png')
-var d = load('imatges/a/player_23.png')
-var esquerra = load('imatges/a/player_14.png')
 
 func _physics_process(_delta):
-	
+	moviment.x=0
 	moviment.y += gravetat
 	
 	if Input.is_action_pressed("ui_right"):
 		moviment.x = velocitat
-		if moviment.x > 0:
-			get_node("Sprite").hide()
-			get_node("AnimatedSprite").show()
-			$AnimatedSprite.play('dreta')
-		else:
-			get_node("Sprite").show()
-			get_node("AnimatedSprite").hide()
-			$Sprite.texture=load(dreta)
-			
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play('caminar')
+	
 	elif Input.is_action_pressed("ui_left"):
 		moviment.x = -velocitat
-		if moviment.x<0:
-			get_node("Sprite").hide()
-			get_node("AnimatedSprite").show()
-			$AnimatedSprite.play('esquerra')
-		else:
-			get_node("Sprite").show()
-			get_node("AnimatedSprite").hide()
-			$Sprite.texture=load(esquerra)	
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play('caminar')
 	else:
-		moviment.x=0
-		get_node("Sprite").show()
-		get_node("AnimatedSprite").hide()
-		$Sprite.texture=d
+		$AnimatedSprite.play('quiet')
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			moviment.y = -1000
