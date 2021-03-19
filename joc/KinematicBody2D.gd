2a849a18ec02d980aefcbd32a9751c9f7c5a7101
Arnpo18 <79510111+Arnpo18@users.noreply.github.com>
2021-03-19 14:34:55 +0100

    cosotos

--- joc/KinematicBody2D.gd
+++ joc/KinematicBody2D.gd
@@ -2,10 +2,12 @@ extends KinematicBody2D
 var moviment = Vector2(0,0)
 const gravetat = 50
 const velocitat = 260
-const ATAC=preload("res://ATAC.tscn")
+const ATAC=preload("res://jugador/ATAC.tscn")
 var pot_disparar = true
 var cooldown=1
 var timer
+var mort = false
+export var hp = 200
 func _ready():
 	timer=Timer.new()
 	add_child(timer)
@@ -13,38 +15,44 @@ func _ready():
 	timer.set_wait_time(cooldown)
 	timer.connect('timeout',self,'cooldown0')
 func _physics_process(_delta):
-	moviment.x=0
-	moviment.y += gravetat
-	if Input.is_action_pressed("ui_right"):
-		moviment.x = velocitat
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play('caminar')
-		if sign($Position2D.position.x) == -1:
-			$Position2D.position.x *= -1
-	elif Input.is_action_pressed("ui_left"):
-		moviment.x = -velocitat
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play('caminar')
-		if sign($Position2D.position.x) == 1:
-			$Position2D.position.x *= -1
-	else:
-		$AnimatedSprite.play('quiet')
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			moviment.y = -750
-	moviment=move_and_slide(moviment, Vector2.UP)
-	if Input.is_action_just_pressed('atacar') && pot_disparar:
-		var bola = ATAC.instance()
-		if sign($Position2D.position.x)==1:
-			bola.direccio_atac(1)
+	if mort == false:
+		moviment.x=0
+		moviment.y += gravetat
+		if Input.is_action_pressed("ui_right"):
+			moviment.x = velocitat
+			$AnimatedSprite.flip_h = false
+			$AnimatedSprite.play('caminar')
+			if sign($Position2D.position.x) == -1:
+				$Position2D.position.x *= -1
+		elif Input.is_action_pressed("ui_left"):
+			moviment.x = -velocitat
+			$AnimatedSprite.flip_h = true
+			$AnimatedSprite.play('caminar')
+			if sign($Position2D.position.x) == 1:
+				$Position2D.position.x *= -1
 		else:
-			bola.direccio_atac(-1)
-		get_parent().add_child(bola)
-		bola.global_position=$Position2D.global_position
-		pot_disparar = false
-		timer.start()
+			$AnimatedSprite.play('quiet')
+		if is_on_floor():
+			if Input.is_action_just_pressed("ui_up"):
+				moviment.y = -750
+		moviment=move_and_slide(moviment, Vector2.UP)
+		if Input.is_action_just_pressed('atacar') && pot_disparar:
+			var bola = ATAC.instance()
+			if sign($Position2D.position.x)==1:
+				bola.direccio_atac(1)
+			else:
+				bola.direccio_atac(-1)
+			get_parent().add_child(bola)
+			bola.global_position=$Position2D.global_position
+			pot_disparar = false
+			timer.start()
 func cooldown0():
 	pot_disparar=true
+func mortj():
+	mort = true
+	moviment.x=0
+	$AnimatedSprite.play('mort')
+	
 
 
 	
---
+++ joc/enemic/Enemic.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+var timer
+var cooldown_i = 1
+const velocitat = 100
+
+var mov = Vector2()
+const gravetat=50
+var moviment = Vector2()
+var direccio=1
+var mort = false
+var hp = 100
+func _ready():
+	pass
+func mort():
+	mort=true
+	moviment=Vector2(0,0)
+	$AnimatedSprite.play('mort')
+	$CollisionShape2D.disabled==true
+	$Timer.start()
+func _physics_process(delta):
+	if mort==false:
+		moviment.x=velocitat*direccio
+		$AnimatedSprite.play('caminar')
+		moviment.y+=gravetat 
+		moviment=move_and_slide(moviment,Vector2.UP)
+		if direccio == 1:
+			$AnimatedSprite.flip_h=false
+		else:
+			$AnimatedSprite.flip_h=true
+		if is_on_wall():
+			direccio=direccio*-1
+			$RayCast2D.position.x *= -1
+		if $RayCast2D.is_colliding()==false:
+			direccio*=-1
+			$RayCast2D.position.x *= -1
+
+
+func _on_Timer_timeout():
+	queue_free() # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	if body.has_method('mortj'):
+		body.hp -= 50
+		$AnimatedSprite.play('atacar')
+		if body.hp <= 0:
+			body.mortj()
+	
